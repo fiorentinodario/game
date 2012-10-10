c55e724ba932b2c04c37eea02b6d0409f51d8ea8
Dario Fiorentino <fiorentinodariogmail.com>
2012-10-10 15:28:07 +0200

    4

--- gen/org/andengine/R.java
+++ gen/org/andengine/R.java
@@ -14,23 +14,32 @@ public final class R {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
         public static final int logo=0x7f020002;
+        public static final int splashscreen=0x7f020003;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int menu_settings=0x7f080001;
+        public static final int splashscreen=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_tmxtiled_map_example=0x7f030000;
+        public static final int main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_tmxtiled_map_example=0x7f060000;
+        public static final int activity_tmxtiled_map_example=0x7f070000;
+    }
+    public static final class raw {
+        public static final int aftertheordeal=0x7f040000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_tmxtiled_map_example=0x7f040003;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
+        public static final int splashscreen=0x7f050004;
+        public static final int title_activity_tmxtiled_map_example=0x7f050003;
+    }
+    public static final class strings {
+        public static final int app_name=0x7f090000;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
